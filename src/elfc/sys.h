95316d82bda4392c1e1714b20ea72de93e89662a
Tony Hefner <tony.hefner@gmail.com>
2025-10-03 22:56:41 -0400

    Add platform dependent path determination

--- src/elfc/sys.h
+++ src/elfc/sys.h
@@ -4,7 +4,7 @@
  */
 
 #define OS		  "Elf/OS"
-#define ASCMD	  "%sasm02 -L -C %s"
+#define ASCMD	  "%sasm02 -I %s -L -C %s"
 #define LDCMD	  "%slink02 -e -S -L %slib -I %slib crt0.prg %s"
 #define SYSLIBC	" -l elfc.lib -l stdlib.lib -l stdio.lib"
 #define ELFLIBC	" -l elfc.lib"
