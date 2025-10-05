c010aa4ea65574c702178d0bdf684bf3638c2d1d
fourstix <fourstix@gmail.com>
2025-10-05 16:03:08 -0400

    Updated code to handle unsigned compares

--- bin/include/limits.h
+++ bin/include/limits.h
@@ -4,11 +4,11 @@
 #define CHAR_BIT   8
 #define CHAR_MIN   0
 #define CHAR_MAX   255
-#define INT_MIN    -32767
+#define INT_MIN    -32768
 #define INT_MAX    32767
-#define SCHAR_MIN  -127
+#define SCHAR_MIN  -128
 #define SCHAR_MAX  127
-#define SHRT_MIN   -32767
+#define SHRT_MIN   -32768
 #define SHRT_MAX   32767
 #define UCHAR_MAX  255
 #define USHRT_MAX  65535
